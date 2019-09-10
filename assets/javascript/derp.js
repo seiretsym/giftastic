f374258b2b02c5de2e53f66a2167c9cfcaae5fda
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-10 11:06:35 -0700

    add: on click event listener for buttons

--- assets/javascript/derp.js
+++ assets/javascript/derp.js
@@ -1,1 +1,16 @@
 // https://api.giphy.com/v1/gifs/random?api_key=wslWpWhssAgYDK6zVXacBDsacT47flr4&tag=cats
+
+// append button to list
+
+// convert string space to plus, so it doesn't break the api url
+
+// button listener
+$("button").on("click", function() {
+
+    if ($(this).attr("id") === "derpSearchBtn") {
+        console.log("search button clicked");
+    }
+    else {
+        console.log("different button clicked");
+    }
+})
